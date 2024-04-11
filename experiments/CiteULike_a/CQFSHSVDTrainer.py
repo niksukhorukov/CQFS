eef5c34c253896c1c1d2d3c3dc0e1b251d6c57cd
Nikita Sukhorukov <nsukhorukov@oseledets-cdise-srv1>
2024-04-11 14:11:02 +0300

    add threading for noisy

--- experiments/CiteULike_a/CQFSHSVDTrainer.py
+++ experiments/CiteULike_a/CQFSHSVDTrainer.py
@@ -5,16 +5,7 @@ from recsys.Recommender_import_list import (
     ItemKNNCFRecommender, PureSVDItemRecommender, RP3betaRecommender,
 )
 
-import ctypes
-def mkl_set_num_threads(num_threads=20):
-    def mkl_set_num_threads(cores):
-        mkl_rt.mkl_set_num_threads(ctypes.byref(ctypes.c_int(cores)))
-        
-    mkl_rt = ctypes.CDLL('libmkl_rt.so')
-    mkl_max_threads = mkl_rt.mkl_get_max_threads()
-    mkl_set_num_threads(num_threads)
-    print(f'[mkl]: set up num_threads={mkl_rt.mkl_get_max_threads()}/{mkl_max_threads}')
-
+from core.mkl import mkl_set_num_threads
 
 
 def main():
